0b9dbb6f79ba62f5ab8be58626f017e8568e5f23
anantoj <gg.ananto@gmail.com>
2022-11-11 16:44:18 +0800

    modified main examples

--- rl_project/main.py
+++ rl_project/main.py
@@ -3,5 +3,10 @@ from rl_project.trainer import Trainer
 # cartpole_trainer = Trainer(mode="pos")
 # cartpole_trainer.train()
 
-mountaincar_trainer = Trainer(mode="pos", env="Acrobot-v1", target_reward=-110)
-mountaincar_trainer.train()
+# mountaincar_trainer = Trainer(mode="pos", env="MountainCar-v0", target_reward=-110)
+# mountaincar_trainer.train()
+
+acrobot_trainer = Trainer(mode="pos", env="Acrobot-v1", target_reward=-110)
+acrobot_trainer.train()
+
+
