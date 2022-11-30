10200487e26179ac01b0f6703cdabca16e12574e
anantoj <gg.ananto@gmail.com>
2022-11-30 16:28:12 +0800

    added 37L

--- rl_project/networks/baseline_model.py
+++ rl_project/networks/baseline_model.py
@@ -318,6 +318,137 @@ class VisionExpand21L(nn.Module):
         return self.head(x.view(x.size(0), -1))
 
 
+class VisionExpand37L(nn.Module):
+    def __init__(self, h , w, outputs):
+        super(VisionExpand37L, self).__init__()
+        self.conv1 = nn.Conv2d(6, 16, kernel_size=5, stride=2)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=5, stride=2)
+        self.bn2 = nn.BatchNorm2d(32)
+        self.conv3 = nn.Conv2d(32, 64, kernel_size=5, stride=2)
+        self.bn3 = nn.BatchNorm2d(64)
+        self.conv4 = nn.Conv2d(64, 128, kernel_size=5, stride=2)
+        self.bn4 = nn.BatchNorm2d(128)
+        self.conv5 = nn.Conv2d(128, 128, kernel_size=5, stride=2)
+        self.bn5 = nn.BatchNorm2d(128)
+        self.conv6 = nn.Conv2d(128, 64, kernel_size=3, stride=1)
+        self.bn6 = nn.BatchNorm2d(64)
+        self.conv7 = nn.Conv2d(64, 32, kernel_size=3, stride=1)
+        self.bn7 = nn.BatchNorm2d(32)
+        self.conv8 = nn.Conv2d(32, 16, kernel_size=3, stride=1)
+        self.bn8 = nn.BatchNorm2d(16)
+        self.conv9 = nn.Conv2d(16, 16, kernel_size=3, stride=1)
+        self.bn9 = nn.BatchNorm2d(16)
+        self.conv10 = nn.Conv2d(16, 32, kernel_size=3, stride=1)
+        self.bn10 = nn.BatchNorm2d(16)
+        self.conv11 = nn.Conv2d(32, 64, kernel_size=4, stride=2)
+        self.bn11 = nn.BatchNorm2d(64)
+        self.conv12 = nn.Conv2d(64, 128, kernel_size=4, stride=2)
+        self.bn12 = nn.BatchNorm2d(128)
+        self.conv13 = nn.Conv2d(128, 128, kernel_size=4, stride=2)
+        self.bn13 = nn.BatchNorm2d(128)
+        
+        self.conv14 = nn.Conv2d(128, 64, kernel_size=4, stride=1)
+        self.bn14 = nn.BatchNorm2d(64)
+        self.conv15 = nn.Conv2d(64, 32, kernel_size=4, stride=1)
+        self.bn15 = nn.BatchNorm2d(32)
+        self.conv16 = nn.Conv2d(32, 16, kernel_size=4, stride=1)
+        self.bn16 = nn.BatchNorm2d(16)
+        self.conv17 = nn.Conv2d(16, 16, kernel_size=4, stride=1)
+        self.bn17 = nn.BatchNorm2d(16)
+        self.conv18 = nn.Conv2d(16, 32, kernel_size=4, stride=1)
+        self.bn18 = nn.BatchNorm2d(16)
+        self.conv19 = nn.Conv2d(32, 64, kernel_size=5, stride=1)
+        self.bn19 = nn.BatchNorm2d(64)
+        self.conv20 = nn.Conv2d(64, 128, kernel_size=5, stride=1)
+        self.bn20 = nn.BatchNorm2d(128)
+        self.conv21 = nn.Conv2d(128, 128, kernel_size=5, stride=1)
+        self.bn21 = nn.BatchNorm2d(128)
+
+        self.conv22 = nn.Conv2d(128, 64, kernel_size=3, stride=1)
+        self.bn22 = nn.BatchNorm2d(64)
+        self.conv23 = nn.Conv2d(64, 32, kernel_size=3, stride=1)
+        self.bn23 = nn.BatchNorm2d(32)
+        self.conv24 = nn.Conv2d(32, 16, kernel_size=3, stride=1)
+        self.bn24 = nn.BatchNorm2d(16)
+        self.conv25 = nn.Conv2d(16, 16, kernel_size=3, stride=1)
+        self.bn25 = nn.BatchNorm2d(16)
+        self.conv26 = nn.Conv2d(16, 32, kernel_size=3, stride=1)
+        self.bn26 = nn.BatchNorm2d(16)
+        self.conv27 = nn.Conv2d(32, 64, kernel_size=4, stride=2)
+        self.bn27 = nn.BatchNorm2d(64)
+        self.conv28 = nn.Conv2d(64, 128, kernel_size=4, stride=2)
+        self.bn28 = nn.BatchNorm2d(128)
+        self.conv29 = nn.Conv2d(128, 128, kernel_size=4, stride=2)
+        self.bn29 = nn.BatchNorm2d(128)
+        
+        self.conv30 = nn.Conv2d(128, 64, kernel_size=4, stride=1)
+        self.bn30 = nn.BatchNorm2d(64)
+        self.conv31 = nn.Conv2d(64, 32, kernel_size=4, stride=1)
+        self.bn31 = nn.BatchNorm2d(32)
+        self.conv32 = nn.Conv2d(32, 16, kernel_size=4, stride=1)
+        self.bn32 = nn.BatchNorm2d(16)
+        self.conv33 = nn.Conv2d(16, 16, kernel_size=4, stride=1)
+        self.bn33 = nn.BatchNorm2d(16)
+        self.conv34 = nn.Conv2d(16, 32, kernel_size=4, stride=1)
+        self.bn34 = nn.BatchNorm2d(16)
+        self.conv35 = nn.Conv2d(32, 64, kernel_size=5, stride=1)
+        self.bn35 = nn.BatchNorm2d(64)
+        self.conv36 = nn.Conv2d(64, 128, kernel_size=5, stride=1)
+        self.bn36 = nn.BatchNorm2d(128)
+        self.conv37 = nn.Conv2d(128, 128, kernel_size=5, stride=1)
+        self.bn37 = nn.BatchNorm2d(128)
+
+        def conv2d_size_out(size, kernel_size=5, stride=2):
+            return (size - (kernel_size - 1) - 1) // stride + 1
+
+        convw = conv2d_size_out(conv2d_size_out(conv2d_size_out(w)))
+        convh = conv2d_size_out(conv2d_size_out(conv2d_size_out(h)))
+        linear_input_size = convw * convh * 32
+        self.head = nn.Linear(linear_input_size, outputs)
+
+    def forward(self, x):
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.relu(self.bn3(self.conv3(x)))
+        x = F.relu(self.bn4(self.conv4(x)))
+        x = F.relu(self.bn5(self.conv5(x)))
+        x = F.relu(self.bn6(self.conv6(x)))
+        x = F.relu(self.bn7(self.conv7(x)))
+        x = F.relu(self.bn8(self.conv8(x)))
+        x = F.relu(self.bn9(self.conv9(x)))
+        x = F.relu(self.bn10(self.conv10(x)))
+        x = F.relu(self.bn11(self.conv11(x)))
+        x = F.relu(self.bn12(self.conv12(x)))
+        x = F.relu(self.bn13(self.conv13(x)))
+        x = F.relu(self.bn14(self.conv14(x)))
+        x = F.relu(self.bn15(self.conv15(x)))
+        x = F.relu(self.bn16(self.conv16(x)))
+        x = F.relu(self.bn17(self.conv17(x)))
+        x = F.relu(self.bn18(self.conv18(x)))
+        x = F.relu(self.bn19(self.conv19(x)))
+        x = F.relu(self.bn20(self.conv20(x)))
+        x = F.relu(self.bn21(self.conv21(x)))
+
+        x = F.relu(self.bn22(self.conv22(x)))
+        x = F.relu(self.bn23(self.conv23(x)))
+        x = F.relu(self.bn24(self.conv24(x)))
+        x = F.relu(self.bn25(self.conv25(x)))
+        x = F.relu(self.bn26(self.conv26(x)))
+        x = F.relu(self.bn27(self.conv27(x)))
+        x = F.relu(self.bn28(self.conv28(x)))
+        x = F.relu(self.bn29(self.conv29(x)))
+        x = F.relu(self.bn30(self.conv30(x)))
+        x = F.relu(self.bn31(self.conv31(x)))
+        x = F.relu(self.bn32(self.conv32(x)))
+        x = F.relu(self.bn33(self.conv33(x)))
+        x = F.relu(self.bn34(self.conv34(x)))
+        x = F.relu(self.bn35(self.conv35(x)))
+        x = F.relu(self.bn36(self.conv36(x)))
+        x = F.relu(self.bn37(self.conv37(x)))
+        return self.head(x.view(x.size(0), -1))
+
+
 
 class VisionExpand6L(nn.Module):
     def __init__(self, h , w, outputs):
@@ -398,6 +529,7 @@ def get_model(model_name, h,w,outputs):
         "VisionExpand6L": VisionExpand6L,
         "VisionExpand13L": VisionExpand13L,
         "VisionExpand21L": VisionExpand21L,
+        "VisionExpand37L": VisionExpand37L,
     }
 
 
